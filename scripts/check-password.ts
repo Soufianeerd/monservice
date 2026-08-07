04c8398caed4cd9f3acb11cb1c11a2a138fd6595
EL RHADI <soufiane.erd@gmail.com>
2026-08-07 11:39:21 +0200

    chore: remove old scripts that cause build errors

--- scripts/check-password.ts
+++
@@ -1,8 +0,0 @@
-import { hashPassword, verifyPassword } from '../src/lib/utils/password';
-
-(async () => {
-  const hash = await hashPassword('password123');
-  console.log('Hash:', hash);
-  const valid = await verifyPassword('password123', hash);
-  console.log('Valid:', valid);
-})();
